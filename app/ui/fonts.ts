7b2450dc91cfa91c34b5be826c7edd8722e31a3e
Aram <armfiletransfer@gmail.com>
2024-04-06 15:09:50 +0400

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Preahvihear } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '700', subsets: ['latin'] });
+export const preahvihear = Preahvihear({ weight: '400', subsets: ['latin'] });
